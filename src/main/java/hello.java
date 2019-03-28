516dac7363eb5fe2a227e732188d349588800461
huaikong06 <wyhuaikong06@163.com>
2019-03-28 15:48:10 +0800

    hello git

---
+++ src/main/java/hello.java
@@ -0,0 +1,13 @@
+/**
+ * @author xxx
+ * @date 2019/3/28 15:46
+ * @description
+ */
+public class hello {
+
+    public static void main(String[] args){
+        System.out.println("蛤蛤");
+    }
+
+
+}
