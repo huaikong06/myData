746ec6e5af8208c74c9f0c36cdecd643824e42c5
huaikong06 <wyhuaikong06@163.com>
2019-03-28 16:29:56 +0800

    hello git2

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -6,7 +6,7 @@
 public class hello {
 
     public static void main(String[] args){
-        System.out.println("蛤蛤");
+        System.out.println("hello");
 
         System.out.println("猴猴猴");
     }
